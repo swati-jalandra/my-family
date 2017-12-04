a65f6a339f8a47cd27dee642d4e14cc40d7dc725
ramprabhu <nramprabu@gmail.com>
2017-12-04 18:36:32 +0530

    Dashboard changes

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,58 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    @users = User.all
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    @users.each do |user|
+      columns do
+        column do
+          @personal_docs = user.personal_docs
+          panel "Personal documents - #{user.name}" do
+            if @personal_docs.present?
+              table_for @personal_docs do |t|
+                t.column("Name") { |doc| doc.name }
+                t.column("Created Date") { |doc| doc.created_at ? l(doc.created_at, :format => :long) : '-' }
+              end
+            else
+               'No documents created yet'
+            end
+          end
+        end
+
+        column do
+          @official_docs = user.official_docs
+          panel "Official documents - #{user.name}" do
+            if @official_docs.present?
+              table_for @official_docs do |t|
+                t.column("Name") { |doc| doc.name }
+                t.column("Created Date") { |doc| doc.created_at ? l(doc.created_at, :format => :long) : '-' }
+              end
+            else
+              'No documents created yet'
+            end
+          end
+        end
+
+        column do
+          @college_docs = user.college_docs
+          panel "College documents - #{user.name}" do
+            if @college_docs.present?
+              table_for @college_docs do |t|
+                t.column("Name") { |doc| doc.name }
+                t.column("Created Date") { |doc| doc.created_at ? l(doc.created_at, :format => :long) : '-' }
+              end
+            else
+              'No documents created yet'
+            end
+          end
+        end
+      end
+    end
+
+
+
   end # content
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :documents, dependent: :destroy
+  has_many :personal_docs, class_name:'PersonalDoc', dependent: :destroy
+  has_many :official_docs, class_name:'OfficialDoc', dependent: :destroy
+  has_many :college_docs, class_name:'CollegeDoc', dependent: :destroy
   validates :name, presence: true, uniqueness: true
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
