222a64b708c407b0323309bf665b0adfb11a4bed
ramprabhu <nramprabu@gmail.com>
2017-12-04 16:15:08 +0530

    Remove my family logo

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -19,7 +19,7 @@ ActiveAdmin.setup do |config|
   #
   # Note: Aim for an image that's 21px high so it fits in the header.
   #
-  config.site_title_image = 'my-family-logo.jpeg'
+  #config.site_title_image = 'my-family-logo.jpeg'
 
   config.comments = false
 
